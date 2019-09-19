f122d6744e3482acf12b15e1342b09d64b41f359
Andrey <amborodin@acm.org>
2019-09-19 15:13:31 +0500

    Even better datediff()

--- yndx_1c_aux--1.0--1.1.sql
+++ yndx_1c_aux--1.0--1.1.sql
@@ -6,24 +6,26 @@ CREATE OR REPLACE FUNCTION datediff(arg_mode character varying,
                                 arg_d1 timestamp without time zone)
 RETURNS integer AS
 $BODY$
+DECLARE
+        mode character varying:= upper(arg_mode)
 BEGIN
-        if arg_mode = 'SECOND' then
+        if mode = 'SECOND' then
                 return date_part('epoch', arg_d1) - date_part('epoch', arg_d2);
-        elsif arg_mode = 'MINUTE' then
+        elsif mode = 'MINUTE' then
                 return ceil((date_part('epoch', arg_d1) - date_part('epoch', arg_d2)) / 60);
-        elsif arg_mode = 'HOUR' then
+        elsif mode = 'HOUR' then
                 return ceil((date_part('epoch', arg_d1) - date_part('epoch', arg_d2)) / 3600);
-        elsif arg_mode = 'DAY' then
+        elsif mode = 'DAY' then
                 return cast(arg_d1 as date) - cast(arg_d2 as date);
-        elsif arg_mode = 'WEEK' then
+        elsif mode = 'WEEK' then
                 return ceil( ( cast(arg_d1 as date) - cast(arg_d2 as date) ) / 7.0);
-        elsif arg_mode = 'MONTH' then
+        elsif mode = 'MONTH' then
                 return 12 * (date_part('year', arg_d1) - date_part('year', arg_d2))
                         + date_part('month', arg_d1) - date_part('month', arg_d2);
-        elsif arg_mode = 'QUARTER' then
+        elsif mode = 'QUARTER' then
                 return 4 * (date_part('year', arg_d1) - date_part('year', arg_d2))
                         + date_part('quarter', arg_d1) - date_part('quarter', arg_d2);
-        elsif arg_mode = 'YEAR' then
+        elsif mode = 'YEAR' then
                 return (date_part('year', arg_d1) - date_part('year', arg_d2));
         else
                 RAISE EXCEPTION 'Unsupported datediff() mode';
